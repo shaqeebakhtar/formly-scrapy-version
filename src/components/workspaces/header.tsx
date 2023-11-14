d00f0da046baf2118b180b4bed7b807e1f3bd0e8
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-11-14 18:03:01 +0530

    add user profile nav and upgrade button in header

--- src/components/workspaces/header.tsx
+++ src/components/workspaces/header.tsx
@@ -1,18 +1,28 @@
-import { Icons } from "../icons";
-import UserProfileDropdown from "./user-profile-dropdown";
+import { Crown } from "lucide-react";
+import { Button } from "../ui/button";
+import UserProfileNav from "./user-profile-nav";
 import WorkSpaceDropdown from "./workspace-dropdown";
 
 type WorkSpacesHeaderProps = {};
 
 export default function WorkSpacesHeader({}: WorkSpacesHeaderProps) {
   return (
-    <div className="flex items-center justify-between border-b border-border p-4">
+    <div className="flex items-center justify-between border-b border-border px-6 py-3 lg:px-8">
       {/* <div className="flex items-center text-lg font-medium">
         <Icons.logo className="mr-2 h-4 w-4" />
         Formly
       </div> */}
       <WorkSpaceDropdown />
-      <UserProfileDropdown />
+
+      <div className="flex items-center gap-6">
+        <Button
+          variant="ghost"
+          className="font-semibold bg-yellow-300 hover:bg-yellow-200"
+        >
+          <Crown className="mr-2 w-4 h-4" /> Upgrade
+        </Button>
+        <UserProfileNav />
+      </div>
     </div>
   );
 }
---
+++ src/components/workspaces/user-profile-nav.tsx
@@ -0,0 +1,61 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CreditCard, LogOut, Settings2, User2 } from "lucide-react";
+
+type UserProfileNavProps = {};
+
+export default function UserProfileNav({}: UserProfileNavProps) {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-9 w-9">
+              <AvatarImage src="/" alt="" />
+              <AvatarFallback className="font-semibold">SA</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end">
+          <DropdownMenuLabel>
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">shaqeeb</p>
+              <p className="text-xs leading-none text-muted-foreground">
+                me@example.com
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User2 className="mr-2 w-4 h-4 opacity-50" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <CreditCard className="mr-2 w-4 h-4 opacity-50" />
+              Billing
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings2 className="mr-2 w-4 h-4 opacity-50" />
+              Settings
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="text-destructive focus:text-destructive focus:bg-destructive/10">
+            <LogOut className="mr-2 w-4 h-4" />
+            Log out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
