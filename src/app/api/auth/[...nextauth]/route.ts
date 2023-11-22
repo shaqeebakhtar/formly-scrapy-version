c2d6fe7df45d177b8bf11e2500ea9fd8bf240476
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-11-22 17:08:16 +0530

    next auth route changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,5 @@
-import prisma from "@/lib/db";
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID as string,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-      version: "2.0",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    jwt: async ({ token, profile }) => {
-      const user = await prisma.user.findFirst({
-        where: {
-          email: profile?.email,
-        },
-      });
-
-      if (user) token.id = user.id;
-
-      return token;
-    },
-
-    session: async ({ session, token }) => {
-      if (session?.user) {
-        session.user.id = token.id as string;
-      }
-
-      return session;
-    },
-  },
-  debug: process.env.NODE_ENV === "development",
-};
+import { authOptions } from "@/lib/auth-options";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
